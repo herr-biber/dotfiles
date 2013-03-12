ad0f7393ab4e191c1a97b0712ba73d02feb62321
Markus Roth <markus.roth@herr-biber.de>
2013-03-12 10:18:46 +0100

    Fix tilda floating.

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -387,7 +387,7 @@ awful.rules.rules = {
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
-    { rule = { name = "tilda" },
+    { rule = { class = "tilda" },
       properties = { floating = true} },
     { rule = { class = "Pidgin" },
       properties = { tag = tags[1][2] } },
