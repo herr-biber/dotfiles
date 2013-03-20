bef14ca229487b7dca8e2689ee24723218965e70
Markus Roth <markus.roth@herr-biber.de>
2013-03-20 11:01:09 +0100

    Change border colors. Gray, green.

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -455,8 +455,8 @@ client.add_signal("manage", function (c, startup)
     end
 end)
 
-client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.add_signal("focus", function(c) c.border_color = "#00cc00" end)
+client.add_signal("unfocus", function(c) c.border_color = "#555555" end)
 -- }}}
 
 -- os.execute("nm-applet &")
