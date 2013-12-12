58180c80b5fca6b7b67160895418ce198004757e
Markus Roth <markus.roth@herr-biber.de>
2013-12-12 19:33:35 +0100

    Add more packages.

--- bin/arch-install.sh
+++ bin/arch-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PACKAGES="ttf-dejavu  opencl-headers gtk2 gtk3 freeglut glu slim awesome vicious rlwrap dex hddtemp alsa-utils tk perl-libwww perl-term-readkey perl-mime-tools perl-net-smtp-ssl perl-authen-sasl subversion cvsps gnome-keyring libgnome-keyring bash-completion rxvt-unicode rxvt-unicode-terminfo gtk2-perl alsa-utils base-devel wget rsync chromium libgnome perl-file-mimeinfo alsa-plugins pulseaudio bluez bluez-libs gconf jack python2-pyqt4 ntfs-3g dosfstools gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-afp gvfs-mtp gvfs-goa libcanberra-pulse libcanberra-gstreamer ffmpeg libva-vdpau-driver sh xf86-input-synaptics xorg-xrdb cmake asciidoc gtest opencv python2-numpy libjpeg-turbo swig fltk boost apr openmpi valgrind gdb glances python2-sensors nss-mdns  dhclient xclip hdf5 python2-nose gedit gedit-plugins vte3 libgit2-glib gtk-engine-unico htop lsof strace autofs sxiv python2-matplotlib pygtk python2-scipy python2-pytest xorg-xrandr acpi bc file-roller p7zip eclipse eclipse-cdt gtk-engine-murrine gnome-themes-standard wxgtk powertop paprefs pavucontrol xorg-xev aspell-en aspell-de ninja jdk chromium-pepper-flash-stable"
+PACKAGES="ttf-dejavu opencl-headers gtk2 gtk3 freeglut glu slim awesome vicious rlwrap dex hddtemp alsa-utils tk perl-libwww perl-term-readkey perl-mime-tools perl-net-smtp-ssl perl-authen-sasl subversion cvsps gnome-keyring libgnome-keyring bash-completion rxvt-unicode rxvt-unicode-terminfo urxvt-perls gtk2-perl alsa-utils base-devel wget rsync chromium libgnome perl-file-mimeinfo alsa-plugins pulseaudio bluez bluez-libs gconf jack python2-pyqt4 ntfs-3g dosfstools gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-afp gvfs-mtp gvfs-goa libcanberra-pulse libcanberra-gstreamer ffmpeg libva-vdpau-driver sh xf86-input-synaptics xorg-xrdb cmake asciidoc gtest opencv python2-numpy libjpeg-turbo swig fltk boost apr openmpi valgrind gdb glances python2-sensors nss-mdns dhclient xclip hdf5 python2-nose gedit gedit-plugins vte3 libgit2-glib gtk-engine-unico htop lsof strace autofs sxiv python2-matplotlib pygtk python2-scipy python2-pytest xorg-xrandr acpi bc file-roller p7zip eclipse eclipse-cdt gtk-engine-murrine gnome-themes-standard wxgtk powertop paprefs pavucontrol xorg-xev aspell-en aspell-de ninja jdk chromium-pepper-flash-stable gimp iotop ipython2 lsb-release odt2txt pidgin pidgin-otr xclip ca-certificates gnupg nano openssh openssl openvpn brasero cups cups-filters cups-pdf doxygen evince sxiv exiv2 firefox flac flashplugin freeglut fuse gnuplot imagemagick inkscape lame lftp libreoffice lm_sensors lsof meld mpg123 mplayer musicbrainz samba smbclient thunar unzip unrar vlc xorg-xkill phatch mendeleydesktop pdftk-bin pmount teamviewer tightvnc ttf-ms-fonts skype"
 
 NOTEBOOK_PACKAGES="nvidia opencl-nvidia networkmanager network-manager-applet samsung-tools"
-DESKTOP_PACKAGES=""
+DESKTOP_PACKAGES="xf86-video-ati"
 
 install_base_devel()
 {
@@ -48,4 +48,4 @@ sudo -n yaourt -Sy
 # install packages
 install_packages $PACKAGES
 
-# enable_services
+# TODO enable_services
