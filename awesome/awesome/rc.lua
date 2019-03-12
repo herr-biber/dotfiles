18afaf62a2dbd7d6cbb4ff3a115a263d28e05737
Markus Roth <markus.r.roth@daimler.com>
2019-03-12 10:05:04 +0100

    Add awesome 4 upstream rc.lua.

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -1,16 +1,13 @@
 -- check for awesome 3.5 onwards
 version_major = tonumber(string.sub(awesome.version, 2, 2))
 version_minor = tonumber(string.sub(awesome.version, 4, 4))
-is_awesome_35 = (version_major == 3 and version_minor >= 5)
 
 awesome.font = "sans 13"
 
-if not is_awesome_35 then 
-    require("rc_2")
-
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
-else
+if version_major >= 4
+    require("rc_4")
+elseif (version_major == 3 and version_minor >= 5):
     require("rc_35")
+else
+    require("rc_2")
 end
