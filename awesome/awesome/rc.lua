e8880187dfd2ea626c63b5c989fb9a868cb0c90c
Markus Roth <markus.r.roth@daimler.com>
2019-03-12 10:12:22 +0100

    Proper lua syntax.

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -4,9 +4,9 @@ version_minor = tonumber(string.sub(awesome.version, 4, 4))
 
 awesome.font = "sans 13"
 
-if version_major >= 4
+if version_major >= 4 then
     require("rc_4")
-elseif (version_major == 3 and version_minor >= 5):
+elseif (version_major == 3 and version_minor >= 5) then
     require("rc_35")
 else
     require("rc_2")
