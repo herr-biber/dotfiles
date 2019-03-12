f3a1d68a629f5d2772da3a5f6be570c0667c6522
Markus Roth <markus.r.roth@daimler.com>
2019-03-12 11:15:50 +0100

    Add alt-tab.

--- awesome/awesome/rc_4.lua
+++ awesome/awesome/rc_4.lua
@@ -275,12 +275,26 @@ globalkeys = gears.table.join(
         end,
         {description = "focus next by index", group = "client"}
     ),
+    awful.key({ "Mod1",           }, "Tab",
+        function ()
+            awful.client.focus.byidx( 1)
+        end,
+        {description = "focus next by index", group = "client"}
+    ),
+
     awful.key({ modkey,           }, "k",
         function ()
             awful.client.focus.byidx(-1)
         end,
         {description = "focus previous by index", group = "client"}
     ),
+    awful.key({ "Mod1", "Shift"   }, "Tab",
+        function ()
+            awful.client.focus.byidx(-1)
+        end,
+        {description = "focus previous by index", group = "client"}
+    ),
+
     awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
               {description = "show main menu", group = "awesome"}),
 
